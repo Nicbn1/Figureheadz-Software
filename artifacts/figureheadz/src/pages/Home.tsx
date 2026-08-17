259bd30160680360bb12bbf26a1fb83751f63a1d
Nicbn1 <nbeni@figureheadz.com>
2026-08-16 20:53:47 -0400

    Home: View All button yellow background

--- artifacts/figureheadz/src/pages/Home.tsx
+++ artifacts/figureheadz/src/pages/Home.tsx
@@ -59,7 +59,7 @@ export default function Home() {
       <section className="container mx-auto px-4">
         <div className="flex items-end justify-between mb-8 border-b-4 border-black pb-4">
           <h2 className="font-display text-5xl uppercase text-white [text-shadow:_-3px_-3px_0_#000,_3px_-3px_0_#000,_-3px_3px_0_#000,_3px_3px_0_#000,_-3px_0px_0_#000,_3px_0px_0_#000,_0px_-3px_0_#000,_0px_3px_0_#000,_5px_5px_0_#000]">Hot Drops</h2>
-          <Button variant="outline" asChild>
+          <Button variant="outline" asChild className="bg-secondary border-black hover:bg-secondary/90">
             <Link href="/shop">View All</Link>
           </Button>
         </div>
